622e9d2cc67a438e687d8a5f1a580b47156cb70f
IceMeril <meril.crouzet05@orange.fr>
2026-04-03 21:11:49 +0200

    Conditions initiales pas finies

--- main.py
+++ main.py
@@ -7,26 +7,41 @@ Tint = 0
 Text = 20
 
 #discrétisation de l'espace
-N = 200
+N = 9
 dx = L/N
 dy = L/N
 X = np.linspace(0, L, N)
 Y = np.linspace(0, L, N)
 
 #Initialisation de C
-C = np.zeros(N,N)
-F = np.zeros(N)
+C = np.zeros((N**2,N**2))
+F = np.zeros((N**2))
 
 #Boucle de remplissage
 
 
 
-#Conditions aux limites
+#Conditions aux limites 
+#Sur F
+for i in range (N):
+    F[i] = Text
+    F[N*i] = Text
+    F[N*(i + 1) -1] = Text
+for i in range(N**2 - N, N**2) :
+    F[i] = Text
 
-#Pour F
+#Sur C
+for i in range(N) :
+    C[i, i] = 1
+    C[N*i, N*i] = 1
+    C[N*(i + 1) -1, N*(i + 1) -1] = 1
 
-#Pour C à gauche
+for i in range(N**2 - N, N**2) :
+    C[i, i] = 1
 
+
+
+print(C)
 #C à droite
 
 #C au milieu
