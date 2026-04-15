26fa8c3d98015abb33b51fc5b7f65dfbb4513ace
Dirtyfire-FR <guillaume.jeanniard@outlook.fr>
2026-04-15 16:17:19 +0200

    suite conditions initiales sur F

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 #déclaration des variables
 L = 1
-Tint = 0
+Tint = 1
 Text = 20
 
 #discrétisation de l'espace
-N = 9
+N = 3
 dx = L/N
 dy = L/N
 X = np.linspace(0, L, N)
@@ -24,11 +24,20 @@ F = np.zeros((N**2))
 #Conditions aux limites 
 #Sur F
 for i in range (N):
-    F[i] = Text
-    F[N*i] = Text
-    F[N*(i + 1) -1] = Text
+    F[i] = Text #la première ligne est à la température de dehors
+    F[N*i] = Text #la première colonne
+    F[N*(i + 1) -1] = Text 
 for i in range(N**2 - N, N**2) :
-    F[i] = Text
+    F[i] = Text # dernière ligne
+
+for i in range(N): #au centre, il y a un trou et la température est différente
+    if i < int(2*N/3) and i >= int(N/3) : #on est dans les lignes centrales 
+        print('true ligne')
+        for j in range(N) :
+            if j%N >= int(N/3) and j%N < int(2*N/3): #colonnes centrales
+                print('true colonne')
+                F[i*N+j] = Tint
+
 
 #Sur C
 for i in range(N) :
@@ -40,14 +49,16 @@ for i in range(N**2 - N, N**2) :
     C[i, i] = 1
 
 
-
 print(C)
+
+print(F)
 #C à droite
 
 #C au milieu
 
 #Résolution 
-
+#U = np.linalg.solve(C,F)
 
 
 #Résultats
+
