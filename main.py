ab4846531b0594a1cb6257c977b84b81d7211b24
Dirtyfire-FR <guillaume.jeanniard@outlook.fr>
2026-05-03 15:51:21 +0200

    ajout mesure du temps de calcul

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time 
 #déclaration des variables
 L = 1
 Tint = 0
 Text = 20
 
 #discrétisation de l'espace
-N = 88
+N = 30
 dx = L/(N-1)
 dy = L/(N-1)
 
@@ -16,18 +16,18 @@ C = np.zeros((N**2,N**2))
 F = np.zeros((N**2))
 
 
-def test_point_intérieur(C, i):
+def test_point_intérieur(i):
     bool = False
     if not((i+1)%N == 0 or (i+1)%N == 1): #le point n'est pas sur les lignes verticales
         if not((i+1) < N or (i+1) > int(N**2)-N): #le point n'est pas sur les lignes horizontales
-            if not((i+1)%N <= 2*N/3 and (i+1)%N > N/3 and (i+1) > len(C)/3 and (i+1) <= 2*len(C)/3) : # le point n'est pas dans le trou du milieu
+            if not((i+1)%N <= 2*N/3 and (i+1)%N > N/3 and (i+1) > (N**2)/3 and (i+1) <= 2*(N**2)/3) : # le point n'est pas dans le trou du milieu
                 bool = True
     return bool
 
 #Boucle de remplissage pour l'équation de chaleur
 compt = 0
 for i in range(len(C)): #on parcoure chaque point de C, ligne par ligne : N**2 points au total avec N points par ligne
-    if test_point_intérieur(C, i) :
+    if test_point_intérieur(i) :
         compt += 1
         C[i,i] = -2*((1/(dx**2))+(1/(dy**2)))
         C[i,i-1] = 1/(dx**2)
@@ -56,8 +56,12 @@ for i in range(N): #au centre, il y a un trou et la température est différente
 
 print (F.reshape(N,N))
 
+t_init = time.time()
 #Résolution 
 U = np.linalg.solve(C,F) #je pourrais faire ça de tête
+t_after = time.time()
+t_calc = t_after - t_init
+print(f"temps de résolution du système : {t_calc} secondes")
 
 #Résultats
 plt.imshow(U.reshape(N,N), cmap='viridis', interpolation='nearest')
